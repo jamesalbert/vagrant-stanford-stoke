1c08815020c4b930dc56a265f70d9666fddf6ced
jamesalbert <jalbert1@uci.edu>
2017-04-18 00:59:41 -0700

    safer commands

--- sources/bootstrap.sh
+++ sources/bootstrap.sh
@@ -13,17 +13,23 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/
 
 # download and install stoke to /home/vagrant/stoke
 # which is synced to the host's $THIS_PROJECT'S_DIR/stoke
-git clone https://github.com/StanfordPL/stoke
-cd stoke
-./configure.sh
-make
-echo 'export PATH="$PATH:/home/vagrant/stoke/bin"' > /home/vagrant/.bashrc
-cd ..
+if [ ! -d stoke ]
+then
+    git clone https://github.com/StanfordPL/stoke
+    cd stoke
+    ./configure.sh
+    make
+    echo 'export PATH="$PATH:/home/vagrant/stoke/bin"' > /home/vagrant/.bashrc
+    cd ..
+fi
 
 # install sana
-chmod 400 /home/vagrant/.ssh/id_rsa
-ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
-git clone git@bitbucket.org:jamesrobertalbert/sana.git
-cd sana
-make
-cd ..
+if [ ! -d sana ]
+then
+    chmod 400 /home/vagrant/.ssh/id_rsa
+    ssh-keyscan bitbucket.org >> /home/vagrant/.ssh/known_hosts
+    git clone git@bitbucket.org:jamesrobertalbert/sana.git
+    cd sana
+    make
+    cd ..
+fi
